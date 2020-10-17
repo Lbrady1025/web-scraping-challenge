bf8ce5105c2034b5b90c72cb555bb507616866e5
Leah Brady <brady.leah@gmail.com>
2020-10-17 19:06:19 -0400

    add app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+app = Flask(__name__)
+
+app.config['MONGO_URI'] = 'mongodb://localhost:27007/mars_app'
+
+@app.route('/scrape')
+def scraper():
+    mars = mongo.db.mars_data
+    mars_data = scrape_mars.scrape()
+    mars.update({}, mars_data, upsert=True)
+    return redirect('/', code=302)
+
+@app.route('/')
+def index():
+    mars = mongo.db.mars_data.find_one()
+    return render_template('index.html', mars_data=mars_data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
